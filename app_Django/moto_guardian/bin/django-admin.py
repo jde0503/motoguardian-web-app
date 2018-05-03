444299c76e5e7b5fb5531a4025f8d1bb76935fba
Bronson Arucan <barucan@ucsd.edu>
2018-05-02 17:47:09 -0700

    added files for web app

---
+++ app_Django/moto_guardian/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/BronsonArucan/Desktop/Django/environments/moto_guardian/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
