c07b2bb023148ce79f1fa8766ecd9eea3a092e6e
Bronson Arucan <barucan@ucsd.edu>
2018-06-01 16:41:02 -0700

    added UI/UX properties to registration page; added button to add a motoguardian device

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,7 @@ from django.contrib.auth import (
     logout,
     )
 from django.forms import ModelForm
+from django.utils.text import slugify
 
 
 class CustomUserCreationForm(forms.Form):
@@ -21,7 +22,7 @@ class CustomUserCreationForm(forms.Form):
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
     #phone_number for arduino
     def clean_username(self):
-        username = self.cleaned_data['username']
+        username = self.cleaned_data['username'].lower()
         r = User.objects.filter(username=username)
         if r.count():
             raise  ValidationError("Username already exists")
@@ -52,14 +53,89 @@ class CustomUserCreationForm(forms.Form):
             last_name = self.cleaned_data['last_name'].capitalize()
         )
         return user
+    # class Meta:
+    #     model = User
+    #     fields = ['Username', 'First Name', 'Last Name','Email', 'Password', 'Re-enter Password']
 
 class DeviceForm(ModelForm):
+    # mg_imei = forms.CharField(label='IMEI', min_length=4, max_length=150)
     
+
     class Meta:
         model = Device
+        
         fields = ['mg_imei','mg_phone','year','make',
         'model','color','cellphone','emergency_name','emergency_number',
         'sensitivity','trip_tracking','current_location','anti_theft']
+        # def clean_username(self):
+        #     mg_imei = slugify(self.cleaned_data['mg_imei'])
+        #     r = Device.objects.filter(mg_imei=mg_imei)
+        #     if r.count():
+        #         raise  ValidationError("fJNFJDNKJDNFKS")
+            
+        # def clean_imei(self,mg_imei):
+        #     mg_imei=slugify(mg_imei)
+        #     # r = Device.objects.filter(mg_imei=mg_imei)
+        #     if Device.objects.filter(mg_imei=mg_imei).exists():
+        #         raise  ValidationError("IMEI already registered")
+        #     return mg_imei
+        # def form_valid(self):
+        #     slug = slugify(self.cleaned_data['mg_imei'])
+        #     try:
+        #         question = Device.objects.get(slug=slug)
+        #     except Device.DoesNotExist:
+        #         raise ValidationError("This question already exist")
+        
+        labels = {
+            'mg_imei':'IMEI',
+            'mg_phone':'MotoGuardian Phone Number',
+            'make': 'Make',
+            'model': 'Model',
+            'year': 'Year',
+            'color':'Color',
+            'cellphone':'Cellphone Number',
+            'emergency_name': 'Emergency Contact Name',
+            'emergency_number':'Emergency Contact Phone Number',
+            'sensitivity':'Sensitivity',
+            'trip_tracking':'Trip Tracking',
+            'current_location':'Current Location',
+            'anti_theft':'Anti-Theft',
+        }
+
+        help_texts = {
+            'mg_imei':'(International Mobile Equipment Identity)',
+            'sensitivity':'(1-10)',
+            'trip_tracking':'Check to enable this feature',
+            'current_location':'Check to enable this feature',
+            'anti_theft':'Check to enable this feature',
+        }
+
+        widgets = {
+            'mg_imei': forms.TextInput(attrs={'class': 'form-control'}),
+            'mg_phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'make': forms.TextInput(attrs={'class': 'form-control'}),
+            'model': forms.TextInput(attrs={'class': 'form-control'}),
+            'year': forms.TextInput(attrs={'class': 'form-control'}),
+            'emergency_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'emergency_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'color': forms.TextInput(attrs={'class': 'form-control'}),
+            'cellphone': forms.TextInput(attrs={'class': 'form-control'}),
+            'sensitivity': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+        error_messages = {
+                    'mg_imei': {
+                        'unique': ("This IMEI is already registered"),
+                    },
+                    'mg_phone': {
+                        'unique': ("This phone number is already registered"),
+                    },
+                }
+        # error_messages = {
+        #     'mg_imei': 'This IMEI a'
+        # }
+
+
 
 
 
