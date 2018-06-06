b9fddebb3772c67dbed596f46bbfaf420671f25d
tmbach <trietmbach@gmail.com>
2018-06-06 15:13:31 -0700

    Initial working Google Map implementation for device card (only 1 card)

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,7 +3,7 @@ function initMap() {
   var location = {lat: latVal, lng: lngVal};
   // The map, centered at location
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: location});
+      document.getElementById('map'), {zoom: 15, center: location});
   // The marker, positioned at location
   var marker = new google.maps.Marker({position: location, map: map});
 }
